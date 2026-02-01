d67d47190cc7c2cc5ab29b4dda583e61f912713d
Roger Weißenbrunner <rogerweissenbrunner@proton.me>
2026-02-01 01:00:00 +0100

    Cleaning up the boilerplate.
    Signed-off-by: Roger Weißenbrunner <rogerweissenbrunner@proton.me>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_19_024027) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_01_000003) do
   create_table "action_mailbox_inbound_emails", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "message_checksum", null: false
